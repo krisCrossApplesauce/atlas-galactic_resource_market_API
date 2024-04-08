5708815304cf886e359ad6aaa824cac23c77c3f6
kris_cross_applesauce <karisrr2203@gmail.com>
2024-04-08 16:06:16 -0500

    not ordering stuff alphabetically

--- data_files/edit_data.py
+++ data_files/edit_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('planet_data.csv')
 
-df.sort_values(by=['name'])
+# df.sort_values(by=['name'])
 
-# df.insert(0, 'id', range(1, len(df) + 1))
+df.insert(0, 'id', range(1, len(df) + 1))
 
 df.to_csv('planet_data.csv', index=False)
